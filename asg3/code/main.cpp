c5b951bbc6eb2f73bfa6f351ad7187bc55c6f1b9
mattk127 <mattk12789@gmail.com>
2020-02-17 17:52:23 -0800

    finished parsing

--- asg3/code/main.cpp
+++ asg3/code/main.cpp
@@ -42,7 +42,6 @@ void parse(string line, str_str_map& map){
   regex eq_value_regex {R"(^\s*=\s*(.*?)\s*$)"};
   regex eq_regex {R"(\s*=\s*$)"};
   str_str_map::iterator itor;
-  //str_str_pair::iterator newPair;
   smatch result;
 
   // '#'
@@ -51,32 +50,52 @@ void parse(string line, str_str_map& map){
     //continue;
   }
   // "key=value"
-  if (regex_search (line, result, key_value_regex)) {
+  if (regex_search(line, result, key_value_regex)) {
     itor = map.find(result[1]);
     str_str_pair::iterator newPair(result[1],result[2]);
     map.insert(newPair);
-    cout << result[1] << " = " << result[2] << endl;
-  }else if(regex_search (line, result, key_regex){
+    cout << result[1] << "=" << result[2] << endl;
+  }else if(regex_search(line, result, key_regex)){
     itor = map.find(result[1]);
     if(itor = map.begin()){
       cout << result[1] << ": key not found" << endl;
+    }else{cout << result[1] << "=" << result[2] << endl;}
+  }else if(regex_search(line, result, key_eq_regex)){
+    itor = map.find(result[1]);
+    if(itor != map.begin()){
+     map.erase(itor);
+    }else{
+      cout << "error: key not found" << endl;
     }
-  })
-    cout << result[1] << "=" << result[2] << endl;
+  }else if(regex_search(line, result, eq_value_regex)){
+    for(auto itor = map.begin();itor != map.end();++itor){
+      if(result[2] == itor->second){
+        cout << itor->first << " = " << itor->second
+        << endl;
+      }
+    }
+  }else{
+    cerr << "Parsing error" << endl;
+  }
+
+
+
+
+    //cout << result[1] << "=" << result[2] << endl;
   //need "=" , "key =", "key", "= value"
 }
 
 int main (int argc, char** argv) {
-  // sys_info::execname (argv[0]);
-  // scan_options (argc, argv);
+  sys_info::execname (argv[0]);
+  scan_options (argc, argv);
 
   str_str_map test;
 
-  // for (char** argp = &argv[optind]; argp != &argv[argc]; ++argp) {
-  //   str_str_pair pair (*argp, to_string<int> (argp - argv));
-  //   cout << "Before insert: " << pair << endl;
-  //   test.insert (pair);
-  // }
+  for (char** argp = &argv[optind]; argp != &argv[argc]; ++argp) {
+    str_str_pair pair (*argp, to_string<int> (argp - argv));
+    cout << "Before insert: " << pair << endl;
+    test.insert (pair);
+  }
 
   // for (str_str_map::iterator itor = test.begin();
   //     itor != test.end(); ++itor) {
