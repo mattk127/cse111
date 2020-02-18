c9f6d55f8abbbbc46ea6aac04f475c766fc9de9e
mattk127 <mattk12789@gmail.com>
2020-02-17 17:32:07 -0800

    updated main

--- asg3/code/main.cpp
+++ asg3/code/main.cpp
@@ -19,69 +19,83 @@ using str_str_map = listmap<string,string>;
 using str_str_pair = str_str_map::value_type;
 
 void scan_options (int argc, char** argv) {
-   opterr = 0;
-   for (;;) {
-      int option = getopt (argc, argv, "@:");
-      if (option == EOF) break;
-      switch (option) {
-         case '@':
-            debugflags::setflags (optarg);
-            break;
-         default:
-            complain() << "-" << char (optopt) << ": invalid option"
-                       << endl;
-            break;
-      }
-   }
+  opterr = 0;
+  for (;;) {
+    int option = getopt (argc, argv, "@:");
+    if (option == EOF) break;
+    switch (option) {
+      case '@':
+        debugflags::setflags (optarg);
+        break;
+      default:
+        complain() << "-" << char (optopt) << ": invalid option" << endl;
+        break;
+    }
+  }
+}
+
+void parse(string line, str_str_map& map){
+  regex comment_regex {R"(^\s*(#.*)?$)"}; //comment
+  regex key_value_regex {R"(^\s*(.*?)\s*=\s*(.*?)\s*$)"};
+  regex key_regex {R"(^\s*([^=]+?)\s*$)"};
+  regex key_eq_regex {R"(^\s*([^=]+?)\s*=\s*$)"};
+  regex eq_value_regex {R"(^\s*=\s*(.*?)\s*$)"};
+  regex eq_regex {R"(\s*=\s*$)"};
+  str_str_map::iterator itor;
+  //str_str_pair::iterator newPair;
+  smatch result;
+
+  // '#'
+  if (regex_search (line, result, comment_regex)) {
+    cout << "Comment" << endl;
+    //continue;
+  }
+  // "key=value"
+  if (regex_search (line, result, key_value_regex)) {
+    itor = map.find(result[1]);
+    str_str_pair::iterator newPair(result[1],result[2]);
+    map.insert(newPair);
+    cout << result[1] << " = " << result[2] << endl;
+  }else if(regex_search (line, result, key_regex){
+    itor = map.find(result[1]);
+    if(itor = map.begin()){
+      cout << result[1] << ": key not found" << endl;
+    }
+  })
+    cout << result[1] << "=" << result[2] << endl;
+  //need "=" , "key =", "key", "= value"
 }
 
 int main (int argc, char** argv) {
-   /*
-   sys_info::execname (argv[0]);
-   scan_options (argc, argv);
-   str_str_map test;
-   for (char** argp = &argv[optind]; argp != &argv[argc]; ++argp) {
-      str_str_pair pair (*argp, to_string<int> (argp - argv));
-      cout << "Before insert: " << pair << endl;
-      test.insert (pair);
-   }
+  // sys_info::execname (argv[0]);
+  // scan_options (argc, argv);
 
-   for (str_str_map::iterator itor = test.begin();
-        itor != test.end(); ++itor) {
-      cout << "During iteration: " << *itor << endl;
-   }
+  str_str_map test;
 
-   str_str_map::iterator itor = test.begin();
-   test.erase (itor);
+  // for (char** argp = &argv[optind]; argp != &argv[argc]; ++argp) {
+  //   str_str_pair pair (*argp, to_string<int> (argp - argv));
+  //   cout << "Before insert: " << pair << endl;
+  //   test.insert (pair);
+  // }
 
-   cout << "EXIT_SUCCESS" << endl;
-   return EXIT_SUCCESS;
-   */
+  // for (str_str_map::iterator itor = test.begin();
+  //     itor != test.end(); ++itor) {
+  //   cout << "During iteration: " << *itor << endl;
+  // }
 
-  //matchlines.cpp
-  
-   regex comment_regex {R"(^\s*(#.*)?$)"};
-   regex key_value_regex {R"(^\s*(.*?)\s*=\s*(.*?)\s*$)"};
-   regex trimmed_regex {R"(^\s*([^=]+?)\s*$)"};
-   for (;;) {
-      string line;
-      getline (cin, line);
-      if (cin.eof()) break;
-      cout << endl << "input: \"" << line << "\"" << endl;
-      smatch result;
-      if (regex_search (line, result, comment_regex)) {
-         cout << "Comment or empty line." << endl;
-         continue;
-      }
-      if (regex_search (line, result, key_value_regex)) {
-         cout << "key  : \"" << result[1] << "\"" << endl;
-         cout << "value: \"" << result[2] << "\"" << endl;
-      }else if (regex_search (line, result, trimmed_regex)) {
-         cout << "query: \"" << result[1] << "\"" << endl;
-      }else {
-         assert (false and "This can not happen.");
-      }
-   }
+  // str_str_map::iterator itor = test.begin();
+  // test.erase (itor);
+
+  // cout << "EXIT_SUCCESS" << endl;
+  // return EXIT_SUCCESS;
+ for (;;) {
+    string line;
+    getline (cin, line);
+    if (cin.eof()) break;
+    cout << endl << "input: \"" << line << "\"" << endl;
+    parse(line,test);
+
+ }
    return 0;
 }
 
