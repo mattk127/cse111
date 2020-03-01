5878e267ed51f923ae2abada3ecc7f257516d884
mattk127 <mattk12789@gmail.com>
2020-03-01 15:33:52 -0800

    cixd done

--- asg4/code/cixd.cpp
+++ asg4/code/cixd.cpp
@@ -49,23 +49,106 @@ void reply_ls (accepted_socket& client_sock, cix_header& header) {
   log << "sent " << ls_output.size() << " bytes" << endl;
 }
 
+void reply_get (accepted_socket& client_sock, cix_header& header) {
+  ifstream file {header.filename};
+  string getFile;
+  char buffer[0x1000];
+
+
+
+  if (file.fail()) {
+    log << "No such file: " << header.filename << endl;
+    header.command = cix_command::NAK;
+    header.nbytes = errno;
+    log << "sending header " << header << endl;
+    send_packet(client_sock, &header, sizeof header);
+    return;
+  }
+
+  file.read (buffer, sizeof buffer);
+  getFile.append (buffer);
+  file.close();
+
+  header.command = cix_command::FILE;
+  header.nbytes = getFile.size();
+  memset (header.filename, 0, FILENAME_SIZE);
+  log << "sending header " << header << endl;
+  send_packet (client_sock, &header, sizeof header);
+  send_packet (client_sock, getFile.c_str(), getFile.size());
+  log << "sent " << getFile.size() << " bytes" << endl;  
+}
+
+
+
+void reply_put(accepted_socket& client_sock, cix_header& header){
+  char buffer[header.nbytes + 1];
+  recv_packet (client_sock, buffer, header.nbytes);
+  ofstream file {header.filename};
+
+
+  if (file.fail()) {
+    log << header.filename << ": ifstream failed: " 
+    << strerror (errno) << endl;
+    header.command = cix_command::NAK;
+    header.nbytes = errno;
+    send_packet (client_sock, &header, sizeof header);
+    return;
+  }
+
+  log << "received " << header.nbytes << " bytes" << endl;
+  file.write (buffer, sizeof buffer);
+  file.close();
+  cout << header.filename << " has been created"<< endl; 
+  header.command = cix_command::ACK;
+  memset (header.filename, 0, FILENAME_SIZE);
+  header.nbytes = 0;
+  log << "sending header " << header << endl;
+  send_packet (client_sock, &header, sizeof header);
+}
+
+void reply_rm (accepted_socket& client_sock, cix_header& header) {
+  int rmFile = unlink (header.filename);
+
+  if (rmFile != 0) {
+    log << "Error: " << header.filename << ": " 
+    << strerror (errno) << endl;
+    header.command = cix_command::NAK;
+    header.nbytes = errno;
+    send_packet (client_sock, &header, sizeof header);
+    return;
+  }
+
+  header.command = cix_command::ACK;
+  header.nbytes = 0;
+  send_packet (client_sock, &header, sizeof header);
+}
+
 
 void run_server (accepted_socket& client_sock) {
   log.execname (log.execname() + "-server");
   log << "connected to " << to_string (client_sock) << endl;
   try {   
-    for (;;) {
-        cix_header header; 
-        recv_packet (client_sock, &header, sizeof header);
-        log << "received header " << header << endl;
-        switch (header.command) {
-          case cix_command::LS: 
-              reply_ls (client_sock, header);
-              break;
-          default:
-              log << "invalid client header:" << header << endl;
-              break;
-        }
+    for (;;){
+      cix_header header; 
+      recv_packet (client_sock, &header, sizeof header);
+      log << "received header " << header << endl;
+      switch (header.command) {
+        case cix_command::LS: 
+          reply_ls (client_sock, header);
+          break;
+        case cix_command::GET: 
+          reply_get (client_sock, header);
+          break;
+        case cix_command::RM: 
+          reply_rm (client_sock, header);
+          break;
+        case cix_command::PUT: 
+          reply_put (client_sock, header);
+          break;
+        default:
+          log << "invalid client header:" << header << endl;
+          break;
+      }
     }
   }catch (socket_error& error) {
     log << error.what() << endl;
@@ -98,10 +181,9 @@ void reap_zombies() {
     int status;
     pid_t child = waitpid (-1, &status, WNOHANG);
     if (child <= 0) break;
-    log << "child " << child
-            << " exit " << (status >> 8)
-            << " signal " << (status & 0x7F)
-            << " core " << (status >> 7 & 1) << endl;
+    log << "child " << child << " exit " 
+    << (status >> 8) << " signal " << (status & 0x7F) 
+    << " core " << (status >> 7 & 1) << endl;
   }
 }
 
@@ -116,8 +198,8 @@ void signal_action (int signal, void (*handler) (int)) {
   sigfillset (&action.sa_mask);
   action.sa_flags = 0;
   int rc = sigaction (signal, &action, nullptr);
-  if (rc < 0) log << "sigaction " << strsignal (signal)
-                    << " failed: " << strerror (errno) << endl;
+  if (rc < 0) log << "sigaction " << strsignal (signal) 
+  << " failed: " << strerror (errno) << endl;
 }
 
 
@@ -129,32 +211,32 @@ int main (int argc, char** argv) {
   in_port_t port = get_cix_server_port (args, 0);
   try {
     server_socket listener (port);
-    for (;;) {
-        log << to_string (hostinfo()) << " accepting port "
-            << to_string (port) << endl;
-        accepted_socket client_sock;
-        for (;;) {
-          try {
-              listener.accept (client_sock);
-              break;
-          }catch (socket_sys_error& error) {
-              switch (error.sys_errno) {
-                case EINTR:
-                    log << "listener.accept caught "
-                        << strerror (EINTR) << endl;
-                    break;
-                default:
-                    throw;
-              }
-          }
-        }
-        log << "accepted " << to_string (client_sock) << endl;
+    for (;;){
+      log << to_string (hostinfo()) << " accepting port " 
+      << to_string (port) << endl;
+      accepted_socket client_sock;
+      for (;;){
         try {
-          fork_cixserver (listener, client_sock);
-          reap_zombies();
-        }catch (socket_error& error) {
-          log << error.what() << endl;
+          listener.accept (client_sock);
+          break;
+        }catch (socket_sys_error& error) {
+            switch (error.sys_errno) {
+              case EINTR:
+                log << "listener.accept caught " 
+                << strerror (EINTR) << endl;
+                break;
+              default:
+                throw;
+            }
         }
+      }
+      log << "accepted " << to_string (client_sock) << endl;
+      try {
+        fork_cixserver (listener, client_sock);
+        reap_zombies();
+      }catch (socket_error& error) {
+        log << error.what() << endl;
+      }
     }
   }catch (socket_error& error) {
     log << error.what() << endl;
